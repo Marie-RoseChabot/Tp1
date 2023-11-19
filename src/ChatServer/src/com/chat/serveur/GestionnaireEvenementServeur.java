531beeca75c1af022cf1c65b3e9078dd9dd4d5e3
YanBurton <yanburton03@gmail.com>
2023-11-19 16:22:59 -0500

    Test more

--- src/ChatServer/src/com/chat/serveur/GestionnaireEvenementServeur.java
+++ src/ChatServer/src/com/chat/serveur/GestionnaireEvenementServeur.java
@@ -5,10 +5,10 @@ import ChatServer.src.com.chat.commun.evenement.GestionnaireEvenement;
 import ChatServer.src.com.chat.commun.net.Connexion;
 
 /**
- * Cette classe reprï¿½sente un gestionnaire d'ï¿½vï¿½nement d'un serveur. Lorsqu'un serveur reï¿½oit un texte d'un client,
- * il crï¿½e un ï¿½vï¿½nement ï¿½ partir du texte reï¿½u et alerte ce gestionnaire qui rï¿½agit en gï¿½rant l'ï¿½vï¿½nement.
+ * Cette classe représente un gestionnaire d'événement d'un serveur. Lorsqu'un serveur reçoit un texte d'un client,
+ * il crée un événement à partir du texte reçu et alerte ce gestionnaire qui réagit en gérant l'événement.
  *
- * @author Abdelmoumï¿½ne Toudeft (Abdelmoumene.Toudeft@etsmtl.ca)
+ * @author Abdelmoumène Toudeft (Abdelmoumene.Toudeft@etsmtl.ca)
  * @version 1.0
  * @since 2023-09-01
  */
@@ -16,90 +16,45 @@ public class GestionnaireEvenementServeur implements GestionnaireEvenement {
     private Serveur serveur;
 
     /**
-     * Construit un gestionnaire d'ï¿½vï¿½nements pour un serveur.
+     * Construit un gestionnaire d'événements pour un serveur.
      *
-     * @param serveur Serveur Le serveur pour lequel ce gestionnaire gï¿½re des ï¿½vï¿½nements
+     * @param serveur Serveur Le serveur pour lequel ce gestionnaire gère des événements
      */
     public GestionnaireEvenementServeur(Serveur serveur) {
         this.serveur = serveur;
     }
 
     /**
-     * Mï¿½thode de gestion d'ï¿½vï¿½nements. Cette mï¿½thode contiendra le code qui gï¿½re les rï¿½ponses obtenues d'un client.
+     * Méthode de gestion d'événements. Cette méthode contiendra le code qui gère les réponses obtenues d'un client.
      *
-     * @param evenement L'ï¿½vï¿½nement ï¿½ gï¿½rer.
+     * @param evenement L'événement à gérer.
      */
     @Override
     public void traiter(Evenement evenement) {
         Object source = evenement.getSource();
         Connexion cnx;
-        String msg, typeEvenement, aliasExpediteur, aliasInviter, aliasToSendMsg, chessPosition;
+        String msg, typeEvenement, aliasExpediteur;
         ServeurChat serveur = (ServeurChat) this.serveur;
 
         if (source instanceof Connexion) {
             cnx = (Connexion) source;
             System.out.println("SERVEUR-Recu : " + evenement.getType() + " " + evenement.getArgument());
             typeEvenement = evenement.getType();
-            switch (typeEvenement) 
-            {
-            case "EXIT": //Ferme la connexion avec le client qui a envoyï¿½ "EXIT":
-                cnx.envoyer("END");
-                serveur.enlever(cnx);
-                cnx.close();
-                break;
-            case "LIST": //Envoie la liste des alias des personnes connectï¿½es :
-                cnx.envoyer("LIST " + serveur.list());
-                break;      
-            case "HIST":
-            	cnx.envoyer("HIST\n"+ serveur.historique());
-            	break;        
-            case "MSG":
-                msg = evenement.getArgument();
-                aliasExpediteur = cnx.getAlias();
-                serveur.ajouterHistorique(aliasExpediteur + ">>" + msg);
-                serveur.envoyerATousSauf(msg, aliasExpediteur);
-                break;
-            case "JOIN": // Envoie une invitation pour un chat priver
-            	aliasInviter = evenement.getArgument().toLowerCase();
-            	aliasExpediteur = cnx.getAlias().toLowerCase();
-            	serveur.sendInvitation(aliasExpediteur, aliasInviter, false);
-            	break;
-            case "DECLINE": // Refuse une invitation pour le chat priver
-            	aliasInviter = evenement.getArgument().toLowerCase();
-            	aliasExpediteur = cnx.getAlias().toLowerCase();
-            	serveur.declineInvitation(aliasExpediteur, aliasInviter);
-            	break;
-            case "INV": // Envoie toutes les utilisateurs qui on envoye une invitation (moi)
-            	aliasExpediteur = cnx.getAlias().toLowerCase();
-            	cnx.envoyer("Les utilisateurs qui t'ont envoye une invitation :\n"+ serveur.historiqueInvitation(aliasExpediteur));
-            	break;
-            case "PRV":// Envoie un message priver a un utilisateur
-                msg = evenement.getArgument().split(" ")[1].toString();
-                aliasExpediteur = cnx.getAlias();
-            	aliasToSendMsg = evenement.getArgument().split(" ")[0].toString().toLowerCase();
-                serveur.envoyerMsgPrive(msg, aliasExpediteur, aliasToSendMsg);
-                break;
-            case "QUIT":// Permet de quitter un salon prive
-            	aliasInviter = evenement.getArgument().toLowerCase();
-                serveur.leaveSalonPriver(aliasInviter);
-                break;
-            case "CHESS":// Permet d'envoyer une demande d'une partie d'echec
-            	aliasInviter = evenement.getArgument().toLowerCase();
-            	aliasExpediteur = cnx.getAlias().toLowerCase();
-            	serveur.sendInvitation(aliasExpediteur, aliasInviter, true);
-                break;
-            case "MOVE":// Permet d'envoyer une demande d'une partie d'echec
-            	chessPosition = evenement.getArgument().toLowerCase();
-            	aliasExpediteur = cnx.getAlias().toLowerCase();
-            	serveur.movePawn(aliasExpediteur, chessPosition);
-                break;
-            case "ABANDON":
-            	aliasExpediteur = cnx.getAlias().toLowerCase();
-            	serveur.giveUp(aliasExpediteur);
-            	break;
-            default: //Renvoyer le texte recu convertit en majuscules :
-                msg = (evenement.getType() + " " + evenement.getArgument()).toUpperCase();
-                cnx.envoyer(msg);
+            switch (typeEvenement) {
+                case "EXIT": //Ferme la connexion avec le client qui a envoyé "EXIT":
+                    cnx.envoyer("END");
+                    serveur.enlever(cnx);
+                    cnx.close();
+                    break;
+                case "LIST": //Envoie la liste des alias des personnes connectées :
+                    cnx.envoyer("LIST " + serveur.list());
+                    break;
+
+                //Ajoutez ici d’autres case pour gérer d’autres commandes.
+
+                default: //Renvoyer le texte recu convertit en majuscules :
+                    msg = (evenement.getType() + " " + evenement.getArgument()).toUpperCase();
+                    cnx.envoyer(msg);
             }
         }
     }
